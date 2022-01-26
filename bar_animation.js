650a132829c07c5f03f5f2231488e0c8ab998703
Nimish-Bhatia <nimishbhatia2002@gmail.com>
2022-01-26 19:24:35 +0530

    bar-speed

--- bar_animation.js
+++ bar_animation.js
@@ -2,7 +2,7 @@ var bar = document.getElementsByTagName("progress");
 var no_of_bars = bar.length;
 for (let i = 0; i <= no_of_bars; i++) {
   var percent = 1;
-  var interval = setInterval(bar_animation, 16);
+  var interval = setInterval(bar_animation, 10);
   const origin_percent = bar[i].value;
   function bar_animation() {
     if (origin_percent <= percent) {
